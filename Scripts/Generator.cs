8a288913d92752cbdf5f8eea10647ee3d25f1849
Mst. Nazneen Aktar <136933385+Web-and-Cloud@users.noreply.github.com>
2024-05-03 23:46:38 +0300

    Add files via upload

---
+++ Scripts/Generator.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public GameObject Seina;
+
+    private void Start()
+    {
+        int leveys = 5;
+
+        for (int rivi = 0; rivi < 1000; rivi++)
+        {
+            for (int sarake = 0; sarake < leveys; sarake++)
+            {
+                Instantiate(Seina, new Vector3(sarake, rivi, 0) + new Vector3(-9, -10, 0), Quaternion.identity);
+            }
+
+            leveys += Random.Range(-1, 2);
+            leveys = Mathf.Clamp(leveys, 2, 8);
+        }
+
+        for (int rivi = 0; rivi < 1000; rivi++)
+        {
+            for (int sarake = 0; sarake < leveys; sarake++)
+            {
+                Instantiate(Seina, new Vector3(-sarake, rivi, 0) + new Vector3(9, -10, 0), Quaternion.identity);
+            }
+
+            leveys += Random.Range(-1, 2);
+            leveys = Mathf.Clamp(leveys, 2, 8);
+        }
+    }
+}
